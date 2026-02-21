efa22c5f8e6a0d188d43fceb8f07d6193323d6d6
Ryota <ryota.akechi@gmail.com>
2026-02-21 09:20:35 +0900

    Add CI workflow and update README for testing strategy

--- app/config.py
+++ app/config.py
@@ -7,5 +7,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev_secret_key_change_me")
-    ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES"), 15)
+    ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES", 15))
     JSON_SORT_KEYS = False
